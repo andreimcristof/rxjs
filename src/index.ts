08a33e0122c2c76374a9bd8a7b5e72db5dac68b9
Andrei C <andreimcristof@gmail.com>
2022-10-29 09:47:50 +0200

    stream tests

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import { interval, timer } from "rxjs";
+import { emitKeypressEvents } from "readline";
 
-timer(1000).pipe(
-  () => console.log("hi");
-);
+emitKeypressEvents(process.stdin);
+if (process.stdin.isTTY) process.stdin.setRawMode(true);
 
+process.stdin.on("keypress", (...ar) => {
+  console.log(JSON.stringify(ar));
+
+  if (ar[1].ctrl && ar[1].sequence === "\u0018") process.exit(0);
+});
